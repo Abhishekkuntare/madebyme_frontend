6eedac97a2c0aba93cf3ab4832dbcf4eba7b514f
Abhishek Kuntare <abhishekkuntare7@gmail.com>
2023-07-06 00:33:49 +0530

    update

--- src/components/Payment/Subscribe.jsx
+++ src/components/Payment/Subscribe.jsx
@@ -1,49 +1,120 @@
-import {
-  Box,
-  Button,
-  Heading,
-  VStack,
-  Container,
-  Text,
-} from '@chakra-ui/react';
 import React from 'react';
+import {
+  chakra,
+  VStack,
+  HStack,
+  Text,
+  Container,
+  Box,
+  Icon,
+  Button,
+  Divider,
+  SimpleGrid,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { BiCheck } from 'react-icons/bi';
+import Testimonials from '../Testimonials/Testimonials';
+
+const plansList = [
+  {
+    title: 'Buy individual courses',
+    subLabel: '3 months subscription',
+    price: 299,
+    features: [
+      '4.4/5 average rating',
+      'Video quality 70%',
+      'Hands-on learning experiences to build your skills',
+      'Career guides for web developers and data scientists',
+      'Course recommendations to help you start learning faster',
+    ],
+  },
+  {
+    title: 'Premium courses',
+    subLabel: '10 months subscription ',
+    price: 799,
+    features: [
+      'Video quality 100%',
+      'Hands-on learning experiences to build your skills',
+      '8,000+ professional and personal development courses',
+      'Doubt Support',
+      'Course recommendations to help you start learning faster',
+    ],
+  },
+];
 
 const Subscribe = () => {
   return (
-    <Container h={'90vh'} p="16">
-      <Heading children="Welcome" my={8} textAlign={'center'} />
-      <VStack
-        boxShadow={'lg'}
-        alignItems={'stretch'}
-        borderRadius="lg"
-        spacing={'0'}
-      >
-        <Box bg={'yellow.400'} p={'4'} css={{ borderRadius: '8px 8px 0 0' }}>
-          <Text color={'black'} children={`Pro Pack - ₹299.00`} />
-        </Box>
-        <Box p="4">
-          <VStack textAlign={'center'} px="8" mt="4" spacing="8">
-            <Text children={`Join pro pack and get access to all content`} />
-            <Heading children={`₹299 Only`} />
-          </VStack>
-          <Button my={8} width="full" colorScheme={'yellow'}>
-            Buy Now
-          </Button>
-        </Box>
-
-        <Box bg={'blackAlpha.600'} p="4" css={{ borderRadius: '0 0 8px 8px' }}>
-          {' '}
-          <Heading
-            size={'xm'}
-            children={`100% refund at cancellation`}
-            color="white"
-            textTransform={'uppercase'}
-          />
-          <Text fontSize={'xs'} color="white" children="*Terms and Condition" />
-        </Box>
-      </VStack>
-    </Container>
+    <>
+      <Container mt={100} maxW="5xl" p="6">
+        <chakra.h2 fontSize="5xl" fontWeight="bold" textAlign="center" mb={5}>
+          Choose your plan
+        </chakra.h2>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2} mt={4}>
+          {plansList.map((plan, index) => (
+            <PricingCard key={index} {...plan} />
+          ))}
+        </SimpleGrid>
+      </Container>
+      <Testimonials />
+    </>
   );
 };
 
 export default Subscribe;
+
+const PricingCard = ({ title, subLabel, price, features }) => {
+  return (
+    <>
+      <Box
+        minW={{ base: 'xs', sm: 'sm' }}
+        border="1px solid"
+        borderColor={useColorModeValue('gray.400', 'gray.600')}
+        rounded="md"
+        marginInline="auto"
+        my={3}
+        _hover={{
+          boxShadow: useColorModeValue(
+            '0 4px 6px rgba(160, 174, 192, 0.6)',
+            '0 4px 6px rgba(9, 17, 28, 0.4)'
+          ),
+        }}
+      >
+        <Box p={6}>
+          <chakra.h2 fontSize="2xl" fontWeight="400">
+            {title}
+          </chakra.h2>
+          <Text fontSize="sm">{subLabel}</Text>
+          <Text fontSize="4xl" fontWeight="bold" my={4}>
+            ₹{price}
+            <Text as="sub" fontSize="md" fontWeight="normal">
+              /month
+            </Text>
+          </Text>
+          <Button
+            colorScheme="yellow"
+            variant="solid"
+            size="md"
+            rounded="md"
+            w="100%"
+          >
+            Get Started
+          </Button>
+        </Box>
+        <Divider />
+        <VStack p={6} spacing={4} alignItems="flex-start">
+          <Text fontSize="sm" fontWeight="semibold">
+            WHAT'S INCLUDED
+          </Text>
+          {features.map((feature, index) => (
+            <HStack key={index} spacing={3}>
+              <Icon as={BiCheck} h={4} w={4} color="green.500" />
+              <Text fontSize="sm" color="gray.500">
+                {feature}
+              </Text>
+            </HStack>
+          ))}
+        </VStack>
+      </Box>
+    </>
+  );
+};
--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,62 +1,10 @@
-import {
-  chakra,
-  Stack,
-  VStack,
-  HStack,
-  Text,
-  Container,
-  Box,
-  Icon,
-  Button,
-  Divider,
-  SimpleGrid,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { chakra, Stack, Text, Container, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import { BiCheck } from 'react-icons/bi';
-
-const plansList = [
-  {
-    title: 'Android Single Site',
-    subLabel: 'Build one application',
-    price: 150,
-    features: [
-      'Authentication',
-      'Tab Bar Navigation',
-      'Push Notifications',
-      'API Integration Built-in',
-      'Easy Upgrades',
-    ],
-  },
-  {
-    title: 'Android Unlimited',
-    subLabel: 'Build unlimited apps',
-    price: 799,
-    features: [
-      'Authentication',
-      'Tab Bar Navigation',
-      'Push Notifications',
-      'API Integration Built-in',
-      'Easy Upgrades',
-      'Build Unlimited Android Apps',
-    ],
-  },
-];
 
 const Pricing = () => {
   return (
     <>
       {' '}
-      <Container maxW="5xl" p="6">
-        <chakra.h2 fontSize="5xl" fontWeight="bold" textAlign="center" mb={5}>
-          Android Pricing
-        </chakra.h2>
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2} mt={4}>
-          {plansList.map((plan, index) => (
-            <PricingCard key={index} {...plan} />
-          ))}
-        </SimpleGrid>
-      </Container>
       <Container maxW={'5xl'} mt={10}>
         <Stack
           textAlign={'center'}
@@ -94,7 +42,7 @@ const Pricing = () => {
               {' '}
               an average salary hike of 89%
             </Text>{' '}
-            after graduating from Crio
+            after graduating from here
           </chakra.h1>
           <Text color={'gray.500'} maxW={'3xl'}>
             A course description is a brief summary of the significant learning
@@ -104,7 +52,7 @@ const Pricing = () => {
           <video
             className="home_video"
             style={{ borderRadius: '4%' }}
-            width={'80%'}
+            width={'100%'}
             muted={true}
             loop={true}
             controls={false}
@@ -114,78 +62,17 @@ const Pricing = () => {
 
           <Stack spacing={6} direction={'row'}>
             <Button
-              rounded={'full'}
               px={6}
               bgGradient="linear(to-r, yellow.400,red.200)"
               _hover={{
                 bgGradient: 'linear(to-r, purple.200,yellow.200)',
               }}
             >
-              <Link to={'/request'}>Get Started</Link>
-            </Button>
-            <Button rounded={'full'} px={6}>
-              <Link to={'/about'}>Learn more</Link>
+              <Link to={'/subscribe'}>Get Subscription</Link>
             </Button>
           </Stack>
         </Stack>
       </Container>
-    </>
-  );
-};
-
-const PricingCard = ({ title, subLabel, price, features }) => {
-  return (
-    <>
-      <Box
-        minW={{ base: 'xs', sm: 'sm' }}
-        border="1px solid"
-        borderColor={useColorModeValue('gray.400', 'gray.600')}
-        rounded="md"
-        marginInline="auto"
-        my={3}
-        _hover={{
-          boxShadow: useColorModeValue(
-            '0 4px 6px rgba(160, 174, 192, 0.6)',
-            '0 4px 6px rgba(9, 17, 28, 0.4)'
-          ),
-        }}
-      >
-        <Box p={6}>
-          <chakra.h2 fontSize="2xl" fontWeight="400">
-            {title}
-          </chakra.h2>
-          <Text fontSize="sm">{subLabel}</Text>
-          <Text fontSize="4xl" fontWeight="bold" my={4}>
-            ${price}
-            <Text as="sub" fontSize="md" fontWeight="normal">
-              /year
-            </Text>
-          </Text>
-          <Button
-            colorScheme="blue"
-            variant="solid"
-            size="md"
-            rounded="md"
-            w="100%"
-          >
-            Get Started
-          </Button>
-        </Box>
-        <Divider />
-        <VStack p={6} spacing={4} alignItems="flex-start">
-          <Text fontSize="sm" fontWeight="semibold">
-            WHAT'S INCLUDED
-          </Text>
-          {features.map((feature, index) => (
-            <HStack key={index} spacing={3}>
-              <Icon as={BiCheck} h={4} w={4} color="green.500" />
-              <Text fontSize="sm" color="gray.500">
-                {feature}
-              </Text>
-            </HStack>
-          ))}
-        </VStack>
-      </Box>
     </>
   );
 };
